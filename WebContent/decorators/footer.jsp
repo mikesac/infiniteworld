b90b0fc6530d449c7c20c6e9bad0fcf903127ea1
Michele Sacchetti <msacchetti@msacchetti-laptop.buongiorno.loc>
2009-03-02 23:58:00 +0100

    new character creation

--- WebContent/decorators/footer.jsp
+++ WebContent/decorators/footer.jsp
@@ -1,1 +1,24 @@
-I'm the footer
+
+<table width="100%" border="0">
+	<tr>
+		<td align="left">
+		<%
+
+if(request.getRemoteUser()!=null){
+	out.write( "Welcome "+request.getRemoteUser());
+}
+else{
+	%>You are not logged in.<%
+}
+
+%>
+</td>
+		<td align="right">
+			<% if(request.getRemoteUser()!=null){
+			 %><a href="<%=request.getContextPath()%>/logout">Logout</a><% 
+			}
+			%>
+			
+		</td>
+	</tr>
+</table>
--- src/org/infinite/web/account/NewCharacter.java
+++ src/org/infinite/web/account/NewCharacter.java
@@ -1,8 +1,9 @@
 package org.infinite.web.account;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -23,66 +24,90 @@ import org.infinite.db.dao.TomcatUsers;
 
 public class NewCharacter extends HttpServlet {
 
-	
+
 	private static final long serialVersionUID = 1L;
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)	throws ServletException, IOException {
-		
-		
+
+
 		String err= "New character created";
-		
+
 		// Create a new file upload handler
 		ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
 
 		// Parse the request
 		try {
 			List<FileItem> items = upload.parseRequest(req);
-			
-			String charName = "anonymous";
+
+			String charName = "";
 			String charPic = "pg_void.png";
 			InputStream is = null;
-			
+			String szFileName = "";
+			String szUser = req.getRemoteUser();
+
 			Iterator iter = items.iterator();			
 			while (iter.hasNext()) {
-			    FileItem item = (FileItem) iter.next();
+				FileItem item = (FileItem) iter.next();
 
-			    if (item.isFormField()) {
-			    	charName = item.getString().trim();
-			    } else {
-			        is = item.getInputStream();
-			        
-			    }
+				if (item.isFormField()) {
+					charName = item.getString().trim();
+				} else {
+					is = item.getInputStream();
+					szFileName = item.getName();
+				}
 			}
+			
+			//TODO validate username
+			if(charName==null || charName.length()==0){
+				throw new Exception("Please insert a valid name for your character!");
+			}
+			
+			
 			if(is!=null && is.available()>0){
-				String now = ""+(new Date()).getTime();
-				charPic = charName+"_"+now;
-			}	
-			
-			List<TomcatUsers> lu = Manager.listByQery("from org.infinite.db.dao.TomcatUsers u where u.user='"+req.getUserPrincipal().getName()+"'");
+				charPic = charName+"_"+szUser;
+
+
+				String szPath = this.getServletContext().getRealPath("/imgs/player/");
+				szFileName = szFileName.substring( szFileName.lastIndexOf(".") );
+				charPic += szFileName;
+				szFileName = szPath +"/"+ charPic;
+
+				File f = new File(szFileName);
+				f.createNewFile();
+				FileOutputStream fos = new FileOutputStream(f);
+				byte[] buf = new byte[1024];
+
+				while( is.read(buf) >0 ){
+					fos.write(buf);
+				}
+				fos.flush();
+				fos.close();
+				is.close();
+			}
+
+			List<TomcatUsers> lu = Manager.listByQery("from org.infinite.db.dao.TomcatUsers u where u.user='"+szUser+"'");
 			List<Area> la = Manager.listByQery("from org.infinite.db.dao.Area u where u.id='1'");
-						
-			Player p = new Player();
-			p.setName(charName);			
+
 			charName = charName.replaceAll(" ", "");			
-			p.setImage( charPic );
-			p.setTomcatUsers( lu.get(0) );
-			p.setArea( la.get(0) );
+			Player p = new Player(lu.get(0),la.get(0),charName,charPic,5,5,5,5,20,5,10,5,1,0,0,0.0f,1,"{\"base\":1}");
 			Manager.create(p);
-			
-			
-				
-				
+
 			
 
-			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/player/character.jsp");
-			dispatcher.forward(req,resp);
-			
 		} catch (FileUploadException e) {
-			// TODO Auto-generated catch block
+			err = e.getMessage();
 			e.printStackTrace();
 		}
+		catch (Exception e) {
+			err = e.getMessage();
+		}
 		req.setAttribute("error", err);
+		//resp.sendRedirect( req.getContextPath() +  "/player/character.jsp");
+		
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/player/character.jsp");
+		dispatcher.forward(req,resp);
 	}
-	
-	}
+
+}
